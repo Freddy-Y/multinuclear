6cea34e110aa3434a39b43f994ae74fec1c8caeb
Freddy-Y <1085671970@qq.com>
2017-06-13 23:19:20 +0800

    Signed-off-by: Freddy-Y <1085671970@qq.com>

---
+++ event.cpp
@@ -0,0 +1,38 @@
+#include<windows.h>
+#include<process.h>
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+HANDLE evRead, evFinish;
+void ReadThread(LPVOID param)
+{
+	WaitForSingleObject(evRead,INFINITE);//始终未激活
+	/*如果指定一个非零值，函数处于等待状态直到hHandle标记的对象被触发，或者时间到了。
+	如果dwMilliseconds为0，对象没有被触发信号，函数不会进入一个等待状态，它总是立即返回。
+	如果dwMilliseconds为INFINITE，对象被触发信号后，函数才会返回。*/
+
+	cout << "Reading" << endl;
+	SetEvent(evFinish);
+}
+
+void WriteThread(LPVOID param)
+{
+	cout << "Writing" << endl;
+	SetEvent(evRead);//激发evRead事件，变为true
+}
+
+int main(int argc,char *argv[])
+{
+	evRead = CreateEvent(NULL,FALSE,FALSE,NULL);  //第二个参数false表示自动
+	evFinish = CreateEvent(NULL,FALSE,FALSE,NULL);
+
+	_beginthread(ReadThread,0,NULL);  //创建线程
+	_beginthread(WriteThread, 0, NULL);
+
+	WaitForSingleObject(evFinish,INFINITE);
+	cout << "The Program is End" << endl;
+
+	return 0;
+}
--- global.cpp
+++ global.cpp
@@ -4,24 +4,29 @@
 #include <windows.h> 
 #include <iostream> 
 using namespace std;
-int globalvar = false;//全局
-DWORD WINAPI ThreadFunc(LPVOID pParam)
+int globalvar = false;//全局变量的定义
+DWORD WINAPI ThreadFunc(LPVOID pParam)  //线程函数 功能：输出字符串，挺200毫秒，设置全局变量为true
 {
-	cout << "ThreadFunc" << endl;
-	Sleep(200);
+	cout << "ThreadFunc 子线程开始创建" << endl;
+	Sleep(2000);
 	globalvar = true;
 	return 0;
 }
+
+/*
 int main()
 {
-	HANDLE hthread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);
-	if (!hthread)
+	HANDLE hthread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);  //创建线程，返回句柄
+	Sleep(1000);
+	if (!hthread)//异常判断
 	{
 		cout << "Thread Create Error ! " << endl;
 		CloseHandle(hthread);
 	}
 	while (!globalvar)
-		cout << "Thread while" << endl;
-	cout << "Thread exit" << endl;
+		cout << "Thread while  正在创建子线程......" << endl;
+	cout << "Thread exit 子线程创建完成" << endl;
+	system("pause");
 	return 0;
 }
+*/
